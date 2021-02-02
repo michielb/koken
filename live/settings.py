0525f76f07432283ce552793eee7c4975df28559
michielb <michielb@mac.com>
2021-02-02 14:29:45 +0100

    Beter

--- live/settings.py
+++ live/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ['SECRET_KEY'] # Instead of your actual secret key
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['koken.michielb.com']
+ALLOWED_HOSTS = ['koken.michielb.com','127.0.0.1']
 
 
 # Application definition
