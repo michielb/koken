bfe10638efa5021571ed0f6d81d756082330fb5f
michielb <michielb@mac.com>
2021-02-02 14:08:35 +0100

    Secret key handled

--- live/settings.py
+++ live/settings.py
@@ -10,17 +10,24 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
+import dotenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
+
+# Update secret key
+SECRET_KEY = os.environ['SECRET_KEY'] # Instead of your actual secret key
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'aylz@f1tz=inpk1s^s02b#!p5ij+u!^=69)*q6o$i!voe=o91x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
